fe4bdd4b15371e4aab5843c1267e672510bd00ca
shubam <chshivam148@gmail.com>
2025-02-10 22:14:12 +0530

    Created Server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import http from 'http';
+import app from './app.js';
+
+
+const server=http.createServer(app);
+
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
