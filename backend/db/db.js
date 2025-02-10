80bc17d13a81a039c86800a5c104b07f11ba4a2e
shubam <chshivam148@gmail.com>
2025-02-10 22:32:45 +0530

    Setting up database

---
+++ backend/db/db.js
@@ -0,0 +1,16 @@
+// Connection of database
+
+import mongoose from 'mongoose'
+
+function connect(){
+    mongoose.connect(process.env.MONGO_URI)
+    then(()=>
+    {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err=>{
+        console.log(err);
+        })
+}
+
+export default connect;
