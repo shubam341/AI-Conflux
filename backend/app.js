0ad329582993aa40849c0f3b1dba288436447310
shubam <chshivam148@gmail.com>
2025-02-10 22:08:24 +0530

    Setting local host

--- backend/app.js
+++ backend/app.js
@@ -3,3 +3,13 @@ dotenv.config()
 import express from 'express';
 
 
+const app=express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+});
+
+export default app;
