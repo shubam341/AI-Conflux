a2889bc8f816cfe8dcfc86db3e855819da35056b
shubam <chshivam148@gmail.com>
2025-02-10 22:27:47 +0530

    Server setup complete

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
-
 import express from 'express';
+import morgan from 'morgan';
 
-
+//App installation
 const app=express()
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
