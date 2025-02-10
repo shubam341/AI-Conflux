a515827b0f1f8020664632350b208bb6a092274a
shubam <chshivam148@gmail.com>
2025-02-10 22:16:43 +0530

    Running Local host file

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ dotenv.config()
 import http from 'http';
 import app from './app.js';
 
+const port=process.env.PORT || 3000;
 
 const server=http.createServer(app);
 
 server.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+    console.log(`Server is running on port${port}`);
 })
