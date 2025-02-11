dd65ee262a91e609753c22668dbccb74f17302e5
shubam <chshivam148@gmail.com>
2025-02-11 22:16:47 +0530

    Completing one end point

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import * as userController from '../controllers/user.controller.js';
+import {body} from 'express-validator'
 
 const router=Router();
 
 
-
+router.post('/register',
+    body('email').isEmail().withMessage('Email must be a valid email address'),
+    body('password').isLength({min:4}).withMessage('Password must be atleast 4 charcter long'),
+    userController.createUserController);
 
 
 export default router;
