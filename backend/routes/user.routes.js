26ee28a27d7760da0d80ac50814b8bc649c03178
shubam <chshivam148@gmail.com>
2025-02-11 23:28:16 +0530

    Setting Set router

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import * as userController from '../controllers/user.controller.js';
 import {body} from 'express-validator'
+import * as authMiddleware from '../middleware/auth.middleware.js'
 
 const router=Router();
 
@@ -15,7 +16,7 @@ router.post('/register',
     body('password').isLength({min:4}).withMessage('Password must be atleast 4 charcter long'),
     userController.loginController);
 
-    router.post('/login',userController.ProfileController);
+    router.get('/login',userController.ProfileController);
 
 
 export default router;
