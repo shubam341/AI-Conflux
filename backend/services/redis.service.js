248c2e22f7e1d6d2d42f8350dd6293602335258c
shubam <chshivam148@gmail.com>
2025-02-14 13:35:13 +0530

    Commited redis client

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -4,8 +4,8 @@ import Redis from 'ioredis'
 //creating redis client
 const redisClient=new Redis({
     host: process.env.REDIS_HOST,
-    port:process.env.REDIS_PASSWORD,
-    password:process.env.REDIS_PASSWORD
+    port: Number(process.env.REDIS_PORT), // Use REDIS_PORT instead of REDIS_PASSWORD
+    password: process.env.REDIS_PASSWORD
 })
 
 //for redis connectiom
