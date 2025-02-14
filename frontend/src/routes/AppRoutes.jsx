701340e33c9f7537185391c6fc047a38accd15a8
shubam <chshivam148@gmail.com>
2025-02-14 16:22:00 +0530

    creating routes of user login

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import{Route,BrowserRouter,Routes} from 'react-router-dom'
 
 
 const AppRoutes=()=>{
   return (
-    <div>App</div>
+  <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<div>Home</div>}/>
+        <Route path="/login" element={<div>login</div>}/>
+        <Route path="/Register" element={<div>Register</div>}/>
+        
+        
+        
+       </Routes>
+  </BrowserRouter>
   )
 }
 
