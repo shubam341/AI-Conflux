1b45ac339ef841df72fe2e0640f50fef51e05da7
shubam <chshivam148@gmail.com>
2025-02-14 16:23:10 +0530

    Comming to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import AppRoutes from "./routes/AppRoutes";
 
 
 const App=()=>{
   return (
-    <div>App</div>
+    <AppRoutes/>
   )
 }
 
