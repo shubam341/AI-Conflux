5b25a15eb25e2f4181e102e70454214e4ad04b20
shubam <chshivam148@gmail.com>
2025-02-14 13:45:42 +0530

    Commited logout token in redis

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -69,3 +69,17 @@ export const ProfileController=async(req,res)=>{
         })
       }
 }
+
+
+export const logoutController =async(req,res)=>{
+    try{
+
+        const token=req.cookies.token || req.headers.authorization.split('')[1];
+
+        redisClient.set(token,'logout','EX',60*60*24)
+
+    }catch(err){
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+}
