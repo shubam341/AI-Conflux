bb28aa25264bd6031c21178a3cc047d3b14165e7
shubam <chshivam148@gmail.com>
2025-02-14 16:46:17 +0530

    Setup login page

---
+++ frontend/src/screens/login.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const login=()=>{
+    return(
+      <div className='min-h-screen flex item-center justify-center bg-gray-900'></div>
+    )
+}
+
+export default login
