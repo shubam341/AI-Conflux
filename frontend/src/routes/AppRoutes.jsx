796541724c3f818d12bf1dcc74ab47d979840ac2
shubam <chshivam148@gmail.com>
2025-02-15 04:16:34 +0530

    created login page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-
+import Login from '../screens/login'
 
 const AppRoutes=()=>{
   return (
   <BrowserRouter>
        <Routes>
         <Route path="/" element={<div>Home</div>}/>
-        <Route path="/login" element={<div>login</div>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/Register" element={<div>Register</div>}/>
         
         
