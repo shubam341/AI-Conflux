39ff73f51453a49483fde020abef633785648bb9
shubam <chshivam148@gmail.com>
2025-02-15 14:18:53 +0530

    Added Onchnge function

--- frontend/src/screens/register.jsx
+++ frontend/src/screens/register.jsx
@@ -5,19 +5,30 @@ import { Link ,useNavigate} from 'react-router-dom';
 import axios from '../config/axios' 
 
 const Register = () => {
+
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-900 p-4'>
       <div className='bg-gray-800 p-8 rounded-2xl  shadow-2xl w-full max-w-md'>
         <h2 className='text-3xl font-bold text-white mb-8 text-center'>Register</h2>
         <form>
+
+
+
           <div className='mb-6'>
             <label className='block text-gray-400 mb-2' htmlFor="email">Email</label>
-            <input type="email" id="email" className='w-full px-4 py-3 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
+            <input
+            onChange={(e)=>setEmail(e.target.value)}
+            type="email" id="email" className='w-full px-4 py-3 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
           </div>
 
           <div className='mb-6'>
             <label className='block text-gray-400 mb-2' htmlFor="password">Password</label>
-            <input type="password" id="password" className='w-full px-4 py-3  rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
+            <input
+            onChange={(e)=>setPassword(e.target.value)}
+            type="password" id="password" className='w-full px-4 py-3  rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
           </div>
 
           <button type='submit' className='w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-200'>Register</button>
