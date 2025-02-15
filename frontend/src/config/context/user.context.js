0879ac90cb61d96fdeb7b617b2a0c8a6de899567
shubam <chshivam148@gmail.com>
2025-02-15 16:31:48 +0530

    Changes user Context

--- frontend/src/config/context/user.context.js
+++ frontend/src/config/context/user.context.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, {createContext,useState,useContext} from 'react'
 
@@ -8,5 +9,17 @@ const UserContext=createContext();
 //Create . Provide componenet
 
 export const UserProvider=({children})=>{
-    
+    const[user,setUser]=useState(null);
+
+    return(
+        <UserContext.Provider value={{user,setUser}}>
+        {children}
+        </UserContext.Provider>
+
+    )
+}
+
+//custom hook to use the USercontext
+export const useUSer=()=>{
+    return useContext(UserContext);
 }
