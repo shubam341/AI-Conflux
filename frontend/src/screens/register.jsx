c26275afa16ca08d412ba1fe218942aa8252ff86
shubam <chshivam148@gmail.com>
2025-02-15 13:32:19 +0530

    Chansses register pages css

--- frontend/src/screens/register.jsx
+++ frontend/src/screens/register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-900 p-4'>
       <div className='bg-gray-800 p-8 rounded-2xl  shadow-2xl w-full max-w-md'>
-        <h2 className='text-3xl font-bold text-white mb-8 text-center'>Login</h2>
+        <h2 className='text-3xl font-bold text-white mb-8 text-center'>Register</h2>
         <form>
           <div className='mb-6'>
             <label className='block text-gray-400 mb-2' htmlFor="email">Email</label>
@@ -19,11 +19,11 @@ const Register = () => {
             <input type="password" id="password" className='w-full px-4 py-3 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
           </div>
 
-          <button type='submit' className='w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-200'>Login</button>
+          <button type='submit' className='w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-200'>Register</button>
         </form>
 
         <p className='text-gray-400 mt-6 text-center'>
-          Already have an account? <Link to="/login" className='text-blue-500 hover:underline'>login</Link>
+          Already have an account? <Link to="/login" className='text-blue-500 hover:underline'>Login</Link>
         </p>
       </div>
     </div>
