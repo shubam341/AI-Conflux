b6090719bcd61dea89ca0fb0e25a5341f16d5baa
shubam <chshivam148@gmail.com>
2025-02-15 16:27:32 +0530

    Setting  user Context

---
+++ frontend/src/config/context/user.context.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React, {createContext,useState,useContext} from 'react'
+
+
+//create the UserContext
+const UserContext=createContext();
+
+//Create . Provide componenet
+
+export const UserProvider=({children})=>{
+    
+}
