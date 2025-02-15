77924108eed804e05e70302c98926fe0569478b1
shubam <chshivam148@gmail.com>
2025-02-15 13:42:17 +0530

    Add home to routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/login'
 import Register from "../screens/register";
+import Home from "../screens/Home";
 
 const AppRoutes=()=>{
   return (
   <BrowserRouter>
        <Routes>
-        <Route path="/" element={}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         
