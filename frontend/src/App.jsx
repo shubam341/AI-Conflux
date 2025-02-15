8d193e0e0d844f36554889bfc470dfbe7169c126
shubam <chshivam148@gmail.com>
2025-02-15 16:34:37 +0530

    completely setup user context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { UserProvider } from "./config/context/user.context";
 
 
 const App=()=>{
-  return (
-    <AppRoutes/>
-  )
+<UserProvider>
+  <AppRoutes/>
+</UserProvider>
 }
 
 export default App
