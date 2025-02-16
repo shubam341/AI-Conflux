57068f5392f4c3714d50b5f54542ac37a5eb6d1e
shubam <chshivam148@gmail.com>
2025-02-16 18:26:30 +0530

    Adding error validation

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -4,5 +4,19 @@ import {validationResult} from 'express-validator'
 
 
 export const createProject=async(req,res)=>{
-    
+    const errors=validationResult(req);
+
+        
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+
+    // }
+    //     try{
+    //        const project=await projectService.createProject(req.body)
+    //        res.status(201).json(project)
+
+
+    //        } catch (error){
+    //         res.status(400).send(error.message);
+    //        } 
 }
