2c6e44df5fc56ef0106123c51efaea4f198bffd9
shubam <chshivam148@gmail.com>
2025-02-16 19:27:37 +0530

    addind middle ware for project creating

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import {body} from 'express-validator'
 import * as projectController from '../controllers/project.controller.js'
-
+//project creatd by only logged in user
+import * as authMiddleWare from '../middlewares/auth.middleware.js';
 
 const router =Router();
 router.post('/create',
+    authMiddleWare.authUser,
     body('name').isString().withMessage("Name is required"),
     projectController.createProject
 )
