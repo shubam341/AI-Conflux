f8f240f7b2060f4f6927b174574871e2ba674eb6
shubam <chshivam148@gmail.com>
2025-02-16 18:23:39 +0530

    importing models

---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,8 @@
+import projectModel from '../models/project.model'
+import projectService from '../service/project.service'
+import {validationResult} from 'express-validator'
+
+
+export const createProject=async(req,res)=>{
+    
+}
