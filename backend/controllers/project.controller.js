233795bbde3beeae7ab1ff2a95ed97cb0bb36f2a
shubam <chshivam148@gmail.com>
2025-02-16 18:35:03 +0530

    Created try statement in controller

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -12,20 +12,17 @@ export const createProject=async(req,res)=>{
         return res.status(400).json({errors:errors.array()});
 
     }
-    //     try{
-    //        const project=await projectService.createProject(req.body)
-    //        res.status(201).json(project)
-
-
-    //        } catch (error){
-    //         res.status(400).send(error.message);
-    //        } 
-    // }
-
-
+try{
     //taking names 
     const {name}=req.body;
     const loggedInUser=await userModel.findOne({email});
     const userId=loggedInUser._id;
 
+    //service used to create project
+    const newProject=await projectService.createProject({name,userId});
+
+    res.status(201).json(newProject)
+}catch(err){
+    res.status(400).send(err.message);
+}
 }
