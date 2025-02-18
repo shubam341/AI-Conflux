de694822eb54634ba59ea8e0cdcaf80fce6c1880
shubam <chshivam148@gmail.com>
2025-02-18 15:17:43 +0530

    creating modal in home page

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,20 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {UserContext} from '../context/user.context'
 
 const Home=()=>{
 
     const {user}=useContext(UserContext)
+    const[isModalOpen,setIsModalOpen]=useState(false)
+
+    //function to create project
+    function createProject(){
+  console.log('create project')
+    }
 
     return(
           <main className="p-4" >
 
             <div className="projects">
-                <div className="project">
+               <button className="project p-4 border border-slate-300 rounded-md">
                 <i className="ri-link"></i>
-                </div>
+                </button>
             </div>
             
+    {/* create modal */}
+
+      {isModalOpen &&(
+        <div className=""></div>
+      )}
+         
           </main>
     )
 }
