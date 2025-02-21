93c901b62f98901b3fda411bbcb6ca997e72319b
shubam <chshivam148@gmail.com>
2025-02-22 01:09:19 +0530

    adding express Validator to controller

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -62,3 +62,15 @@ export const getAllProject=async(req,res)=>{
     
     }
 }
+
+
+
+//Setting up express validator
+export const addUserToProject=async(req,res)=>{
+    const errors=validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+}
+
