78c40e51fb33c71da287c31bfc0d8f41fde4e3ac
shubam <chshivam148@gmail.com>
2025-02-24 14:20:56 +0530

    Using use lovstion

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,15 +4,18 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/login'
 import Register from "../screens/register";
 import Home from "../screens/Home";
+import Project from "../screens/project";
 
 
 const AppRoutes=()=>{
   return (
   <BrowserRouter>
        <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/login" element={<Login/>}/>
        <Route path="/register" element={<Register/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/project" element={<Project/>}/>
+
        
         
         
--- frontend/src/screens/project.jsx
+++ frontend/src/screens/project.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 
 const Project =()=>{
+    
+    const location=useLocation()
+
+console.log(location.state)
     return(
         <div>Pri</div>
     )
