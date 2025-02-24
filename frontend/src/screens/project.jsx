ee63af5ee4ac7b76cced686b548102cbdca452a9
shubam <chshivam148@gmail.com>
2025-02-25 02:30:55 +0530

    creting front end of homepage

--- frontend/src/screens/project.jsx
+++ frontend/src/screens/project.jsx
@@ -11,7 +11,16 @@ console.log(location.state)
     return(
      <main className="h-screen w-screen flex">
 
-    <section className="left h-full min-w-100 bg-red-300"></section>
+    <section className="left h-full min-w-100 bg-red-300">
+
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-200">
+        
+        <button className="p-2  ">
+        <i className="ri-group-fill"></i>
+        </button>
+
+        </header>
+    </section>
      </main>
     )
 }
