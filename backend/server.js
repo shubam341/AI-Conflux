d224ff1166919cb575b54caf94fccc152c3b13f5
shubam <chshivam148@gmail.com>
2025-02-28 02:42:30 +0530

    Creted middleware in sockiet.io

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,27 @@ const server=http.createServer(app);
 //sockiet.io
 const io = new Server(server);
 
+//middleware for sockiet that only authenticated user can connectesd
+io.use((socket,next)=>{
+    try{
+      const token=socket.handshake.auth.token || socket.handshake.headers.authorization?.split('')[1];
+
+      if('token'){
+        return next(new Error('Authentication error'))
+      }
+      const decoded=jwt.verify(token, process.env.JWT_SECRET);
+
+      if(!decoded){
+        return next(new Error('Authentication error'))
+      }
+
+      socket.user=decoded;
+      next();
+    } catch(error){
+        next(error)
+    }
+})
+
 io.on('connection', socket => {
     console.log('a user connected');
   socket.on('event', data => { /* … */ });
