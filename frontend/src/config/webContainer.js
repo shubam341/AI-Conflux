51bda8e1e8ae9a9c7a0db9384c1d691d5279c702
shubam <chshivam148@gmail.com>
2025-03-11 19:26:39 +0530

    Adding file to webcontainer

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -3,3 +3,12 @@ import { WebContainer } from '@webcontainer/api';
 // // Call only once
 // const webcontainerInstance = await WebContainer.boot();
 
+let webContainerInstance=null;
+
+export const getWebContainer=()=>{
+    if(webContainerInstance===null){
+        webContainerInstance=new WebContainer();
+
+    }
+    return webContainerInstance;
+}
